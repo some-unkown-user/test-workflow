119336c91ffedd8fbb45d044b0a9d2e57be79f01
Si Beaumont <simon.beaumont@citrix.com>
2012-12-11 11:28:22 +0000

    Fix unit test path

--- test/romantest.py
+++ test/romantest.py
@@ -17,7 +17,7 @@ import sys
 import os
 import unittest
 
-sys.path.append(os.path.join(os.getcwd(), '../src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 import roman
 
 class KnownValues(unittest.TestCase):
